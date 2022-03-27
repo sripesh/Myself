c65c51e935b2d6fb3c88a0f4add4258243d35258
Amarjit Sripesh <thunderworriers@gmail.com>
2022-03-27 14:55:55 +0530

    Update Hello.java

--- Java/Hello.java
+++ Java/Hello.java
@@ -2,6 +2,8 @@ package Java;
 class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World.");
+        System.out.println("First Name : "+args[0]);
+        System.out.println("Last Name : "+args[1]);
+        System.out.println("Age : "+Integer.parseInt(args[2]));
     }
 }
