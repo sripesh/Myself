aca737cb0f9f28800a8eaabd081f43d1797fd968
Amarjit Sripesh <thunderworriers@gmail.com>
2022-03-29 22:10:45 +0530

    Create Prime.java

---
+++ Java/Prime.java
@@ -0,0 +1,20 @@
+package Java;
+import java.util.Scanner;
+public class Prime {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n=scan.nextInt();
+        scan.close();
+        for(int i=2;i<Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                System.out.println("Number "+n+" is a composite number.");
+                return;
+            }
+        }
+        System.out.println("Number "+n+" is a prime number.");
+        return;
+    }
+}
