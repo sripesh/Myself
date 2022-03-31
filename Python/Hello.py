291ba138f93fe15147326ff0fd5e0c97a9eee947
Amarjit Sripesh <thunderworriers@gmail.com>
2022-03-31 22:04:58 +0530

    Update Hello.py

--- Python/Hello.py
+++ Python/Hello.py
@@ -1,7 +1,5 @@
 from tkinter import *
 root=Tk()
-grid1=Label(root,text="Hello")
-grid2=Label(root,text="Welcome Here")
-grid1.grid(row=0,column=0)
-grid2.grid(row=1,column=0)
+grid1=Label(root,text="Hello!").grid(row=0,column=0)
+grid2=Label(root,text="Welcome Here.").grid(row=1,column=1)
 root.mainloop()
