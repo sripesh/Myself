6ba28ee69431e024804db9b98c98002c9ecf9da2
Amarjit Sripesh <thunderworriers@gmail.com>
2022-04-20 13:41:15 +0530

    Update Prime.java

--- Java/Prime.java
+++ Java/Prime.java
@@ -6,7 +6,7 @@ public class Prime {
         System.out.print("Enter a number : ");
         int n=scan.nextInt();
         scan.close();
-        for(int i=2;i<Math.sqrt(n);i++)
+        for(int i=2;i<=Math.sqrt(n);i++)
         {
             if(n%i==0)
             {
