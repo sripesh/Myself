995ad0f13324f681f7fd487f676e5032659151c4
Amarjit Sripesh <thunderworriers@gmail.com>
2022-05-03 23:09:06 +0530

    Create Fibonacci.py

---
+++ Python/Basics/Fibonacci.py
@@ -0,0 +1,10 @@
+def fib(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    return fib(n-1)+fib(n-2)    
+n=int(input("Enter the number of elements:"))
+print("Fibonacci series:")
+for i in range(n):
+    print(fib(i),end=' ')
